9d6e8ed30a141312e4a1771b3e5b408065d34409
Edward Krayer <github@edwardkrayer.com>
2022-04-02 15:25:34 -0700

    Fixed bad system in build script.

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ memThresholdVeryLow='2000'
 memThresholdLow='6000'
 memFree=$(free -m | grep "Mem" | awk '{print $4+$6}')
 
-if [ $memFree -lt $memThresholdVeryLow  ]
+if [ $memFree -lt $memThresholdVeryLow  ]; then
 
         echo "  Using very low memory Dockerfile"
         docker build --tag=input-leap:build . < Dockerfile.very-low-memory
 
-elif [ $memFree -lt $memThresholdLow  ]
+elif [ $memFree -lt $memThresholdLow  ]; then
 
         echo "  Using low memory Dockerfile"
         docker build --tag=input-leap:build . < Dockerfile.low-memory
