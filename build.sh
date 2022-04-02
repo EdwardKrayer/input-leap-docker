f374066e02792cee852b8480a324f6e4d5038872
Edward Krayer <github@edwardkrayer.com>
2022-04-02 15:18:58 -0700

    Updated build.sh to check for low and very low memory.

--- build.sh
+++ build.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-docker build --tag=input-leap:build . < Dockerfile
+memThresholdVeryLow='2000'
+memThresholdLow='6000'
+memFree=$(free -m | grep "Mem" | awk '{print $4+$6}')
+
+if [ $memFree -lt $memThresholdVeryLow  ]
+
+        echo "  Using very low memory Dockerfile"
+        docker build --tag=input-leap:build . < Dockerfile.very-low-memory
+
+elif [ $memFree -lt $memThresholdLow  ]
+
+        echo "  Using low memory Dockerfile"
+        docker build --tag=input-leap:build . < Dockerfile.low-memory
+
+else
+
+        echo "  Using normal Dockerfile"
+        docker build --tag=input-leap:build . < Dockerfile
+
+fi
+
 mkdir ./src/
-buildpath=$(realpath ./src)
+buildpath=$(realpath ./src/)
+
 docker run \
         --tty \
         --volume=$buildpath:/tmp/install/ \
